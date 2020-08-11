066abaafddfdcf3b5f4806473f3e2a88079c1272
vallka <vallka@vallka.com>
2020-08-11 21:58:48 +0100

    ...

--- dhl/api_views.py
+++ dhl/api_views.py
@@ -11,4 +11,5 @@ class DHLViewset(viewsets.ModelViewSet):
     serializer_class = DHLSerializer
 
     def list(self, request):
+        print(len(self.queryset))
         return super().list(self, request)
