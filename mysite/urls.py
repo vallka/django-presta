3602d75591a172a51ca1320438c4945b99d9b407
vallka <vallka@vallka.com>
2020-08-12 11:53:42 +0100

    ..

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
 
 from rest_framework import permissions
 
@@ -22,6 +23,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
 #from rest_framework.schemas import get_schema_view
+
 
 from .api import router
 
