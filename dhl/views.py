7eb9919e772e4f2232f5213876aa9e6b701079f7
vallka <vallka@vallka.com>
2020-09-06 17:05:29 +0100

    serializer

--- dhl/views.py
+++ dhl/views.py
@@ -23,7 +23,104 @@ class DHLSerializer(serializers.ModelSerializer):
         model = DHLParcel
         fields = '__all__'
 
-class UPSSerializer(serializers.ModelSerializer):
+class UPSSerializer0(serializers.ModelSerializer):
     class Meta:
         model = UPSParcel
         fields = '__all__'
+
+class UPSSerializer(serializers.BaseSerializer):
+    def to_representation(self, instance):
+        return {
+            "ShipmentRequest": {
+                "Shipment": {
+                    "Description": instance.Description,
+                    "ReferenceNumber" : {
+                        "Value": instance.ReferenceNumber
+                    },
+                    "Shipper": {
+                        "Name": "GellifiQue Ltd",
+                        "AttentionName": "Margaryta",
+                        "TaxIdentificationNumber": "",
+                        "Phone": {
+                            "Number": "447746358920"
+                        },
+                        "ShipperNumber": "2V813A",
+                        "Address": {
+                            "AddressLine": "41 Deantown Avenue Whitecraig",
+                            "City": "Musselburgh",
+                            "StateProvinceCode": "East Lothian",
+                            "PostalCode": "EH21 8NS",
+                            "CountryCode": "GB"
+                        }
+                    },
+                    "ShipTo": {
+                        "Name": instance.ShipTo_Name,
+                        "AttentionName": instance.ShipTo_AttentionName,
+                        "EMailAddress": instance.ShipTo_EMailAddress,
+                        "Phone": {
+                            "Number": instance.ShipTo_Phone_Number
+                        },
+                        "Address": {
+                            "AddressLine": instance.ShipTo_Address_AddressLine1,
+                            "AddressLine": instance.ShipTo_Address_AddressLine2,
+                            "City": instance.ShipTo_Address_City,
+                            "PostalCode": instance.ShipTo_Address_PostalCode,
+                            "CountryCode": instance.ShipTo_Address_CountryCode
+                        }
+                    },
+                    "ShipFrom": {
+                        "Name": "GellifiQue Ltd",
+                        "AttentionName": "Margaryta",
+                        "TaxIdentificationNumber": "",
+                        "Phone": {
+                            "Number": "07746358920"
+                        },
+                        "ShipperNumber": "0001",
+                        "Address": {
+                            "AddressLine": "41 Deantown Avenue Whitecraig",
+                            "City": "Musselburgh",
+                            "PostalCode": "EH21 8NS",
+                            "CountryCode": "GB"
+                        }
+                    },
+                    "PaymentInformation": {
+                        "ShipmentCharge": {
+                            "Type": "01",
+                            "BillShipper": {
+                                "AccountNumber": "2V813A"
+                            }
+                        }
+                    },
+                    "Service": {
+                        "Code": "11",
+                        "Description": "Standard"
+                    },
+                    "Package": [
+                        {
+                            "Description": "Manicure Accessories",
+                            "Packaging": {
+                                "Code": "02"
+                            },
+                            "PackageWeight": {
+                                "UnitOfMeasurement": {
+                                    "Code": "KGS"
+                                },
+                                "Weight": instance.Package_Weight
+                            },
+                            "PackageServiceOptions": ""
+                        }
+                    ],
+                    "ItemizedChargesRequestedIndicator": "",
+                    "RatingMethodRequestedIndicator": "",
+                    "TaxInformationIndicator": "",
+                    "ShipmentRatingOptions": {
+                        "NegotiatedRatesIndicator": ""
+                    }
+                },
+                "LabelSpecification": {
+                    "LabelImageFormat": {
+                        "Code": "GIF"
+                    }
+                }
+            }
+        }
