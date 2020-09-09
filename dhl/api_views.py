a72f3e5023a634f38b59511a412ca402052dc5e6
vallka <vallka@vallka.com>
2020-09-09 12:24:42 +0100

    ..

--- dhl/api_views.py
+++ dhl/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets,generics
-from rest_framework.views import APIView
+from rest_framework.views import APIView,ListAPIView
 from rest_framework.permissions import IsAuthenticated 
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
@@ -66,7 +66,7 @@ class UPSListView(generics.ListAPIView):
         return Response(serializer.data)                
 
 
-class UPSAction(APIView):
+class UPSAction(ListAPIView):
     parser_class = (JSONParser,)
     serializer_class = UPSSerializer
 
