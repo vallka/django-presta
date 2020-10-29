b148ec29f640d06bec95e43bd2e3ee9894d4c857
vallka <vallka@vallka.com>
2020-10-29 20:54:42 +0000

    subdomain blog

--- mysite/settings.py
+++ mysite/settings.py
@@ -174,9 +174,8 @@ MARKDOWNX_IMAGE_MAX_SIZE = { 'size': (610, 610), 'quality': 80 }
 MARKDOWNX_MARKDOWN_EXTENSIONS = [
     'tables'
 ]
-MARKDOWNX_URLS_PATH = '/pyadmin734r04xdw/markdownify/'
-MARKDOWNX_UPLOAD_URLS_PATH = '/pyadmin734r04xdw/upload/'
-
+#MARKDOWNX_URLS_PATH = '/pyadmin734r04xdw/markdownify/'
+#MARKDOWNX_UPLOAD_URLS_PATH = '/pyadmin734r04xdw/upload/'
  
 
 REST_FRAMEWORK = {
