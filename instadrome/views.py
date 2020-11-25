c904a5c56585cd5d8d61f9c260224d11c4b65696
vallka <vallka@vallka.com>
2020-11-25 15:56:18 +0000

    ..

--- instadrome/views.py
+++ instadrome/views.py
@@ -5,6 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.shortcuts import redirect
+from django.http import HttpResponse
 
 # Create your views here.
 def ig_image(request):
@@ -14,6 +15,9 @@ def ig_image(request):
 
     logger.error("ig_image - id:%s",id)
     img = f'https://www.instagram.com/p/{id}/'
+
+    text = img`
+
     try:
         page = requests.get(img, allow_redirects=True)
 
@@ -21,11 +25,16 @@ def ig_image(request):
         url = re.search(r'"display_url":"(https://.*?)"',data[1])
         url = url[1].replace(r'\u0026','&')
         print(url)
+
+        text += "\n" + url
+
         logger.error("ig_image - url:%s",url)
 
     except Exception as e:
         logger.error(e)
-        pass
+        text += "\nError:" + e
+
+    return HttpResponse(text)
 
 
     return redirect(url)
