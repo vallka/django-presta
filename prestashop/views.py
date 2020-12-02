d6e0595ae56f3ac761e1a8cf3c2087a3dd00a619
vallka <vallka@vallka.com>
2020-12-02 13:49:54 +0000

    ..

--- prestashop/views.py
+++ prestashop/views.py
@@ -32,7 +32,7 @@ class OrderListView(generic.ListView):
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = Order
-        fields = ('id_order','reference','id_order_state','order_state','shipping_number','firstname_customer','lastname_customer','note','firstname','lastname','email','postcode',
+        readonly_fields = ('id_order','reference','id_order_state','order_state','shipping_number','firstname_customer','lastname_customer','note','firstname','lastname','email','postcode',
                 'address1','address2','city','phone','country','currency_code','total_paid','total_products_wt','total_shipping_tax_incl','date_add','date_upd','id_country','carrier','is_new')
 
     def build_unknown_field(self, field_name, model_class):
