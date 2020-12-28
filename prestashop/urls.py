77e063dce70072afc4b171e789b1bfeb027ee400
vallka <vallka@vallka.com>
2020-12-28 16:06:26 +0000

    ..

--- prestashop/urls.py
+++ prestashop/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('product/', ProductListView.as_view(), name='product'),
     path('order/', OrderListView.as_view(), name='order'),
     path('order/<int:id_order>/', OrderDetailListView.as_view(), name='order_detail'),
+    path('upload-cert/<str:email>/put.method/', putfile, name='putmethod'),
     path('upload-cert/<str:email>/', UploadPageView.as_view(), name='upload-cert'),
     path('customer-cert/<str:email>/', CertListView.as_view(), name='cert-list'),
-    path('upload-cert/<str:email>/put.method/', UploadPageView.as_view(), name='upload-cert'),
 ]
